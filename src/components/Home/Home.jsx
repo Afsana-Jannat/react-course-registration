a90f226d8227a0ddf6fda295a3b765e736438ac3
afsana-jannat <afsanajannat205@gmail.com>
2023-09-15 08:56:39 +0600

    insert detailed message here

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className='card-container'></div>
+            <div className='card'>
+            <div className='card-img'>
+                <img className='photo' src="../src/Asset/Rectangle 2-1.png" alt="" />
+            </div>
+            <h2>antoru</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
