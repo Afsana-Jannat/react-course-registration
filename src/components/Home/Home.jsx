33b80e2683d8ce2bcd4f047c20f710bded355fa7
afsana-jannat <afsanajannat205@gmail.com>
2023-09-15 10:53:08 +0600

    Add user authentication feature

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import "./Home.css"
 
 const Home = () => {
+    const [allCourse, setCourse] = useState([])
+    useEffect(() => {
+        fetch("./data.json")
+            .then(res => res.json())
+            .then((data) => setCourse(data))
+    }, [])
+
+console.log(allCourse)
     return (
         <div className='container'>
-            <div className='card-container'></div>
-            <div className='card'>
-            <div className='card-img'>
-                <img className='photo' src="../src/Asset/Rectangle 2-1.png" alt="" />
-            </div>
-            <h2>antoru</h2>
+            <div className='home-container'>
+                <div className='card-container'>
+                   {
+                    allCourse.map(course => (
+                        <div key={course.Id} className='card'>
+                        <div className='card-img'>
+                            <img className='photo' src={course.cover} alt="" />
+                        </div>
+                        <h2 className='title'>{course.title}</h2>
+                        <p className='description'><small>{course.description}</small></p>
+                        <div className='info'>
+                            <p>Price: {course.price}</p>
+                            <p>Credit: {course.credit}</p>
+                        </div>
+                        <button className='card-btn'>Select</button>
+                    </div>
+                    ))
+                   }
+                </div>
+                <div className='cart'>
+                    <h1>rjl</h1>
+                </div>
             </div>
         </div>
     );
